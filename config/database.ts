5171a61604eb1631107c64a9517d8a9d6db9745d
Anuj Mourya <136978647+anuj0224@users.noreply.github.com>
2024-07-09 14:26:37 +0530

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -3,7 +3,7 @@
 const { Sequelize } = require('sequelize');
 
 // Replace with your database credentials
-export const sequelize = new Sequelize('database_development', 'root', 'anuj@2002', {
+export const sequelize = new Sequelize('database_name', 'username', 'password', {
   host: 'localhost',
   dialect: 'mysql',
 });
