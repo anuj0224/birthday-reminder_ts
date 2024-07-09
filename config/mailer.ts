f2e163f6fdf82938183254b4cce2f8f1e65ab72a
Anuj Mourya <136978647+anuj0224@users.noreply.github.com>
2024-07-09 14:25:55 +0530

    Update mailer.ts

--- config/mailer.ts
+++ config/mailer.ts
@@ -5,11 +5,11 @@ const nodemailer = require('nodemailer');
 // Create a transporter object using SMTP transport
 export const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
+  port: 587,      // or 465
   secure: false, // true for 465, false for other ports
   auth: {
-    user: 'mouryaanuj62@gmail.com',
-    pass: 'yzyfufssdeaydcab',
+    user: 'USER_EMAIL_ADDRESS',
+    pass: 'USER_APP_PASSWORD',
   },
 });
 
